83c14c368a4719076dc6847abe128ef35851d044
Martin Omondi <134892561+Martin-nitram1@users.noreply.github.com>
2023-11-16 01:45:25 +0300

    Updated info_operation.c

--- info_operation.c
+++ info_operation.c
@@ -62,7 +62,7 @@ void free_info(info_t *info, int all)
 		if (!info->cmd_buf)
 			free(info->arg);
 		if (info->env)
-			free_list(&(info_env));
+			free_list(&(info->env));
 		if (info->history)
 			free_list(&(info->alias));
 		ffree(info->environ);
