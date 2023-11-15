21f5ee8ffba7705cb5628e5e0dabf996ef952ac5
Martin Omondi <134892561+Martin-nitram1@users.noreply.github.com>
2023-11-15 15:18:16 +0300

    Updated getenv.c

--- getenv.c
+++ getenv.c
@@ -24,23 +24,23 @@ char **get_environ(info_t *info)
 int _unsetenv(info_t *info, char *var)
 {
 	list_t *node = info->env;
-	size_t i = 0;
-	char *p;
+	size_t j = 0;
+	char *q;
 
 	if (!node || !var)
 		return (0);
 	while (node)
 	{
-		p = starts_with(node->str, var);
-		if (p && *p == '=')
+		q = starts_with(node->str, var);
+		if (q && *q == '=')
 		{
-			info->env_changed = delete_node_at_index(&(info->env), i);
-			i = 0;
+			info->env_changed = delete_node_at_index(&(info->env), j);
+			j = 0;
 			node = info->env;
 			continue;
 		}
 		node = node->next;
-		i++
+		j++
 	}
 	return (info->env_changed);
 }
@@ -54,33 +54,33 @@ int _unsetenv(info_t *info, char *var)
  **/
 int _setenv(info_t *info, char *var, char *value)
 {
-	char *buf = NULL;
+	char *buffer = NULL;
 	list_t *node;
-	char *p;
+	char *q;
 
 	if (!var || !value)
 		return (0);
-	buf = malloc(_strlen(var) + _strlen(value) + 2);
+	buffer = malloc(_strlen(var) + _strlen(value) + 2);
 	if (!buf)
 		return (1);
-	_strcpy(buf, var);
-	_strcat(buf, "=");
-	_strcat(buf, value);
+	_strcpy(buffer, var);
+	_strcat(buffer, "=");
+	_strcat(buffer, value);
 	node = info->env;
 	while (node)
 	{
-		p = starts_with(node->str, var);
-		if (p && *p == '=')
+		q = starts_with(node->str, var);
+		if (q && *q == '=')
 		{
 			free(node->str);
-			bie->str = buf;
+			node->str = buffer;
 			info->env_changed = 1;
 			return (0);
 		}
 		node = node->next;
 	}
-	add_node_end(&(info->env), buf, 0);
-	free(buf);
+	add_node_end(&(info->env), buffer, 0);
+	free(buffer);
 	info->env_changed = 1;
 	return (0);
 }
