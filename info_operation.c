3169b223a3c2c9b8ec7550e90773dd6ccfe24527
Martin Omondi <134892561+Martin-nitram1@users.noreply.github.com>
2023-11-15 18:14:20 +0300

    Update info_operation.c

--- info_operation.c
+++ info_operation.c
@@ -69,7 +69,7 @@ void free_info(info_t *info, int all)
 		info->environ = NULL;
 		bfree((void **)info->cmd_buf);
 		if (info->readfd > 2)
-			clse(info->readfd);
+			close(info->readfd);
 		_putchar(BUF_FLUSH);
 	}
 }
