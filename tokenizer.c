6b64d3b9608764c26065cef5a48db10548fc0d6b
Martin Omondi <134892561+Martin-nitram1@users.noreply.github.com>
2023-11-15 18:35:45 +0300

    Update tokenizer.c

--- tokenizer.c
+++ tokenizer.c
@@ -24,12 +24,12 @@ char **strtow(char *str, char *d)
 	s = malloc((1 + numwords) * sizeof(char *));
 	if (!s)
 		return (NULL);
-	for (is = 0, jk = 0; j < numwords; jk++)
+	for (is = 0, jk = 0; jk < numwords; jk++)
 	{
 		while (is_delim(str[is], d))
 			is++;
 		k = 0;
-		while (!is_Delim(str[is + k], d) && str[is + k])
+		while (!is_delim(str[is + k], d) && str[is + k])
 			k++;
 		s[jk] = malloc((k + 1) * sizeof(char));
 		if (!s[jk])
@@ -40,7 +40,7 @@ char **strtow(char *str, char *d)
 			return (NULL);
 		}
 		for (m = 0; m < k; m++)
-			s[j][m] = str[is++];
+			s[jk][m] = str[is++];
 		s[jk][m] = 0;
 	}
 	s[jk] = NULL;
@@ -63,7 +63,7 @@ char **strtow2(char *str, char d)
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if ((str[i] != d && str[i + 1] == d) ||
-				(str[i] != d && !str[i + 1]) || str[i + 1]\ == d)
+				(str[i] != d && !str[i + 1]) || str[i + 1] == d)
 			numwords++;
 	}
 	if (numwords == 0)
@@ -71,7 +71,7 @@ char **strtow2(char *str, char d)
 	s = malloc((1 + numwords) * sizeof(char *));
 	if (!s)
 		return (NULL);
-	for (i = 0; j = 0; j < numwords; j++)
+	for (i = 0, j = 0; j < numwords; j++)
 	{
 		while (str[i] == d && str[i + k] && str[i + k] != d)
 			k++;
