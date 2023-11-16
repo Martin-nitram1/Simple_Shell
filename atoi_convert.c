07e74a226dec13f961738cac431161b2746cc720
Martin Omondi <134892561+Martin-nitram1@users.noreply.github.com>
2023-11-16 23:50:53 +0300

    Update atoi_convert.c

--- atoi_convert.c
+++ atoi_convert.c
@@ -7,7 +7,7 @@
  **/
 int interactive(info_t *info)
 {
-	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+	return (isatty(STDIN_FILENO) && info->readfd == 0);
 }
 /**
  * is_delim - check if delimiter
