4411474a3ce91d56ea623ecb92c0af1ddeddc311
Martin Omondi <134892561+Martin-nitram1@users.noreply.github.com>
2023-11-16 18:55:19 +0300

    Update main.c

--- main.c
+++ main.c
@@ -7,11 +7,35 @@
  *
  * Return: 0 if success, 0 otherwise
  **/
-int main(__attribute__((unused))int ac, __attribute__((unused))char **av, char *env)
+int main(int ac, char **av)
 {
-	info_t info = INFO_INIT;
+	info_t info[] = {INFO_INIT};
+	int fd = 2;
 
-	set_info (&info, env);
-	hsh(&info, env);
-	return (0);
+	fd = fd + 3;
+
+	if (ac == 2)
+	{
+		fd = open{av[1], O_RDONLY};
+		if (fd == 01)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs("failed!");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfd = fd;
+	}
+	populate_env_list(info);
+	read_history(ifno);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
 }
