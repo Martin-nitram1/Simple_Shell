13ee297bb8236deed0534bb994c7a035f3c6f37e
Martin Omondi <134892561+Martin-nitram1@users.noreply.github.com>
2023-11-16 18:36:31 +0300

    Update main.c

--- main.c
+++ main.c
@@ -7,35 +7,11 @@
  *
  * Return: 0 if success, 0 otherwise
  **/
-int main(int ac, char **av)
+int main(int argc, char **argv, char **env)
 {
-	info_t info[] = {INFO_INIT};
-	int fds = 2;
+	info_t info = INFO_INIT;
 
-	fds = fds + 3;
-
-	if (ac == 2)
-	{
-		fds = open(av[1], O_RDONLY);
-		if (fds == -1)
-		{
-			if (errno == EACCES)
-				exit(126);
-			if (errno == ENOENT)
-			{
-				_eputs(av[0]);
-				_eputs("; 0: Can't open ");
-				_eputs(av[1]);
-				_eputchar('\n');
-				_eputchar(BUF_FLUSH);
-				exit(127);
-			}
-			return (EXIT_FAILURE);
-		}
-		info->readfd = fds;
-	}
-	populate_env_list(info);
-	read_history(info);
-	hsh(info, av);
-	return (EXIT_SUCCESS);
+	set_info (&info, env);
+	hsh(&info, env);
+	return (0);
 }
