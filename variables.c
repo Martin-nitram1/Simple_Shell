1ca68a893e17738640208ce6ffacd75803b6b9fb
Martin Omondi <134892561+Martin-nitram1@users.noreply.github.com>
2023-11-17 04:23:50 +0300

    Update variables.c

--- variables.c
+++ variables.c
@@ -61,10 +61,10 @@ void check_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len)
 		if (!info->status)
 		{
 			buf[i] = 0;
-			j = len;
+			jt = len;
 		}
 	}
-	*p = j;
+	*p = jt;
 }
 /**
  * replace_alias - replace alias in tokenized string
