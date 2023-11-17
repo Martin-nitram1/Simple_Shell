16c4000f51a4f379cbee0144970b62a59dd34f42
Martin Omondi <134892561+Martin-nitram1@users.noreply.github.com>
2023-11-17 05:27:26 +0300

    Update atoi_convert.c

--- atoi_convert.c
+++ atoi_convert.c
@@ -1,22 +1,22 @@
 #include "shell.h"
 /**
- * interactive_mode - check if in interactrive mode
+ * interactive - check if in interactrive mode
  * @info: param
  *
  * Return: 1 if true otherwise 0
  **/
-int interactive_mode(info_t *info)
+int interactive(info_t *info)
 {
 	return (isatty(STDIN_FILENO) && info->readfile == 0);
 }
 /**
- * is_delimiter - check if delimiter
+ * is_delim - check if delimiter
  * @c: char to check
  * @delim: delim string
  *
  * Return: 1 if true 0 if false
  **/
-int is_delimiter(char c, char *delim)
+int is_delim(char c, char *delim)
 {
 	while (*delim)
 		{
@@ -26,12 +26,12 @@ int is_delimiter(char c, char *delim)
 	return (0);
 }
 /**
- * _isalphabetic - is alphanumeric?
+ * _isalpha - is alphanumeric?
  * @c: char to check
  *
  * Return: 1 if true, 0 otherwise
  **/
-int _isalphabetic(int c)
+int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
@@ -39,13 +39,13 @@ int _isalphabetic(int c)
 		return (0);
 }
 /**
- * _atoi - convert string to integer
+ * _erratoi - convert string to integer
  * @s: string to convert
  *
  * Return: 0 if no num in string, otherwise
  * converted number
  **/
-int _atoi(char *s)
+int _erratoi(char *s)
 {
 	int m, sig = 1, wave = 0, out;
 	unsigned int res = 0;
