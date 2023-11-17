9e12ad1eae92fabb6aca5aee9cbf7af0508f1c5c
Martin Omondi <134892561+Martin-nitram1@users.noreply.github.com>
2023-11-18 01:07:23 +0300

    Update getline_operation.c

--- getline_operation.c
+++ getline_operation.c
@@ -21,7 +21,7 @@ ssize_t input_buf(info_t *info, char **buf, size_t *len)
 #if USE_GETLINE_FLAG
 		r = getline(buf, &len_p, stdin);
 #else
-		r = _getline(info, buf, &len_p);
+		r = getline_input(info, buf, &len_p);
 #endif
 		if (r > 0)
 		{
