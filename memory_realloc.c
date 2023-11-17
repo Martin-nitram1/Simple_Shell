287b04adc116cee5746e461faaf11a9ab9af0488
Martin Omondi <134892561+Martin-nitram1@users.noreply.github.com>
2023-11-17 04:20:51 +0300

    Update memory_realloc.c

--- memory_realloc.c
+++ memory_realloc.c
@@ -54,7 +54,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	 return (NULL);
 
  old_size = old_size < new_size ? old_size : new_size;
- while (old_Size--)
+ while (old_size--)
 	 p[old_size] = ((char *)ptr)[old_size];
  free(ptr);
  return (p);
