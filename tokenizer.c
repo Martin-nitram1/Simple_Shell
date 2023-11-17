6bf726cb573c74a5806dcd7e34a8955fbeb45b34
Martin Omondi <134892561+Martin-nitram1@users.noreply.github.com>
2023-11-17 05:43:15 +0300

    Update tokenizer.c

--- tokenizer.c
+++ tokenizer.c
@@ -77,7 +77,7 @@ char **strtow2(char *str, char d)
 		while (str[i] == d && str[i] != d)
 			i++;
 		k = 0;
-		while (str[i + k] !=d && str[i + k] && str[i + k] != d)
+		while (str[i + k] != d && str[i + k] && str[i + k] != d)
 			k++;
 		s[j] = malloc((k + 1) * sizeof(char));
 		if (!s[j])
@@ -87,7 +87,7 @@ char **strtow2(char *str, char d)
 			free(s);
 			return (NULL);
 		}
-		for (m = 0; m <k; m++)
+		for (m = 0; m < k; m++)
 			s[j][m] = str[i++];
 		s[j][m] = 0;
 	}
